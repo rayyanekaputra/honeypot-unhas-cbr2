eb118fada0aeec622b4cdbc68ac68d409913f724
rayyanekaputra <bullzhit99@gmail.com>
2024-06-20 14:41:30 +0800

    styles: frontend flex

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,52 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/login', { username, password });
+      await axios.post("http://localhost:5000/api/login", {
+        username,
+        password,
+      });
       // alert('Login attempt recorded.');
     } catch (error) {
-      alert('An error occurred.');
+      alert("An error occurred.");
     }
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Honeypot Login</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Username:
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-          </label>
-          <br />
-          <button type="submit">Login</button>
-        </form>
-      </header>
+      <h1>
+        UNHAS's <br />
+        Admin Panel
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-field">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
